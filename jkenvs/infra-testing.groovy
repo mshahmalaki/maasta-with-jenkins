f8c83d2d90008c28c148b4c96cc790f6d7a4c98e
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2022-02-20 23:45:29 +0330

    update INFRA_CONFIG_GIT_URL

--- jkenvs/infra-testing.groovy
+++ jkenvs/infra-testing.groovy
@@ -1,2 +1,2 @@
-env.INFRA_CONFIG_GIT_URL = "http://192.168.1.10:3000/Infrastructure/infra-testing-config.git"
+env.INFRA_CONFIG_GIT_URL = "http://192.168.1.10:3000/infra-secrets/infra-testing-config.git"
 env.INFRA_CONFIG_GIT_CREDENTIAL_ID = "git-user-bot"
