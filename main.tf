0cab27911cbd06b9982284b349789e5ce027f58a
mshahmalaki <mshahmalaki@gmail.com>
2022-02-27 01:07:31 +0330

    add tags as variable

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "maas_instance" "testing" {
   count               = 2
   release_erase       = false
   release_erase_quick = true
-  tags                = var.MAAS_MACHINE_TAGS
+  tags                = [var.MAAS_MACHINE_TAGS]
 }
