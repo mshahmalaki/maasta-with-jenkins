a1d502b5fb9ce4ba3fd2689fda6f737f03224214
mshahmalaki <mshahmalaki@gmail.com>
2022-02-26 10:14:04 +0330

    change git url

--- jkenvs/infra-testing.groovy
+++ jkenvs/infra-testing.groovy
@@ -1,2 +1,2 @@
-env.INFRA_CONFIG_GIT_URL = "http://192.168.1.10:3000/infra-config/infra-testing-config.git"
+env.INFRA_CONFIG_GIT_URL = "http://192.168.121.14:3000/infra-config/infra-testing-config.git"
 env.INFRA_CONFIG_GIT_CREDENTIAL_ID = "git-user-bot"
