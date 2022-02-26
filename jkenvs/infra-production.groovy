644a6326f0ab30355643773f3f2dbe61f2c925c2
mshahmalaki <mshahmalaki@gmail.com>
2022-02-27 02:09:02 +0330

    separate environments

---
+++ jkenvs/infra-production.groovy
@@ -0,0 +1,2 @@
+env.INFRA_CONFIG_GIT_URL = "http://192.168.121.14:3000/infra-config/infra-production-config.git"
+env.INFRA_CONFIG_GIT_CREDENTIAL_ID = "git-user-bot"
---
+++ jkenvs/infra-staging.groovy
@@ -0,0 +1,2 @@
+env.INFRA_CONFIG_GIT_URL = "http://192.168.121.14:3000/infra-config/infra-staging-config.git"
+env.INFRA_CONFIG_GIT_CREDENTIAL_ID = "git-user-bot"
