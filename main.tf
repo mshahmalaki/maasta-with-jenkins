57b26eb53382657b3ce2aa66f237c5a1a7670ecb
mshahmalaki <mshahmalaki@gmail.com>
2022-02-28 14:30:44 +0330

    add user_data for cloud init

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "maas_instance" "my_instance" {
   release_erase       = false
   release_erase_quick = true
   tags                = var.MAAS_MACHINE_TAGS
+  user_data           = file("${path.module}/user_data/preinstall_packages.yml")
 }
