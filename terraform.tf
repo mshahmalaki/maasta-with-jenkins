c4d872922f7c09c9fd237d65e4dcdaeee22c5272
mshahmalaki <mshahmalaki@gmail.com>
2022-02-28 11:56:53 +0330

    move backend

--- terraform.tf
+++ terraform.tf
@@ -4,30 +4,4 @@ terraform {
       source = "suchpuppet/maas"
     }
   }
-  backend "s3" {
-    endpoint                    = "http://192.168.121.14:9000"
-    bucket                      = "terraform"
-    region                      = "main"
-    key                         = "terraform.tfstate"
-    access_key                  = "5f94956791a8d768"
-    secret_key                  = "02e3250e6d7a1b56"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
-    workspace_key_prefix        = "tfstate"
-  }
-  backend "s3" {
-    endpoint                    = "http://192.168.121.14:9000"
-    bucket                      = "terraform"
-    region                      = "main"
-    key                         = "terraform.plan"
-    access_key                  = "5f94956791a8d768"
-    secret_key                  = "02e3250e6d7a1b56"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
-    workspace_key_prefix        = "tfstate"
-  }
 }
